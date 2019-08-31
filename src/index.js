1c405c9256ab679eeb1e39f1a4471db52e847bd7
azmeirovitz <arcomp123@gmail.com>
2019-08-31 15:33:00 -0700

    JSX | React Intro - Ariel Meirovitz

--- src/index.js
+++ src/index.js
@@ -1,7 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+
+const luckyNumber = () => {
+
+    const num = Math.floor((Math.random() * 1000) +1);
+
+    return num;
+    console.log("num: ", num);
+
+    // function myFunction() {
+//     var x = Math.floor((Math.random() * 10) + 1);
+//     document.getElementById("demo").innerHTML = x;
+//}  
+}
+
+const Greeting = (parameter) => {
+    
+    return (
+        <div className="container">
+            <h1>
+                Welcome {parameter.name}
+            </h1>
+
+            <h2 className="text-muted">
+               Your lucky number is: {parameter.luckyNumber} 
+            </h2>
+
+        </div>
+    )
+
+        
+}
+
+// export default Greeting. //No need to export here, nothing was chained.
+
+
+const user = {
+    name: "", 
+    luckyNumber: luckyNumber()
+}
+
+
+
 ReactDOM.render(
-    <h1 className="container text-center">React Prototypes!</h1>,
+    <Greeting {...user} />, //this tells to RENDER the component Greeting
+    
     document.getElementById('root')
 );
+
+
